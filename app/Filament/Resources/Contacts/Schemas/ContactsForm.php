dee02bc252ab187d6005251257d27b2c58235f25
moeez <moeez00591@gmail.com>
2025-12-22 15:01:04 +0500

    Add phone input validation and Filament phone input
    Integrated ysfkaya/filament-phone-input package and its assets for improved phone number input in contacts. Updated ContactsForm to enforce phone number format and length, and added a migration to restrict the phone column to 11 characters.

--- app/Filament/Resources/Contacts/Schemas/ContactsForm.php
+++ app/Filament/Resources/Contacts/Schemas/ContactsForm.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Contacts\Schemas;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
+use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
 
 class ContactsForm
 {
@@ -18,8 +19,13 @@ class ContactsForm
                ->maxLength(255),
                TextInput::make('phone')
                ->label('Phone')
+                ->type('tel')
+                ->maxLength(11)
+                ->minLength(11)
+                ->regex('/^03[0-9]{9}$/')
+                ->prefixIcon('heroicon-o-phone')
+                ->numeric()
                ->required()
-               ->maxLength(255)
                ->unique(),
                Select::make('ctype')
                 ->options([

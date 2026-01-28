bb1bbe4f40648ae7863b619ce50488bae4331eaf
moeez <moeez00591@gmail.com>
2026-01-28 16:03:17 +0500

    Fix: Remove duplicate widget registration (auto-discovery handles it)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -67,9 +67,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                SuperAdminStatsWidget::class,
-                AgencyOwnerStatsWidget::class,
-                InvestorStatsWidget::class
+            
             ])
             ->middleware([
                 EncryptCookies::class,
